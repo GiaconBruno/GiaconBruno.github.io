bcae8bb89a2218b2353616483f216b39f6f3cc51
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-05 22:06:48 -0300

    att

--- js/axios.js
+++ js/axios.js
@@ -28,7 +28,7 @@ function dados() {
 
                 var li = document.createElement('li');
                 content.appendChild(li);
-                var list = `<a href="${html_url}/index.html">${name}</a> - ${description}`;
+                var list = `<i class="fas fa-check mr-3"></i><a href="./${name}">${name}</a> - ${description}`;
                 li.innerHTML = list;
                 cont++;
             });
