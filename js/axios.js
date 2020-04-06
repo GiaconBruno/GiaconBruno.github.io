844a41dc42aa68dc21ae2dc9245e8239f9fb145f
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-05 22:44:26 -0300

    att

--- js/axios.js
+++ js/axios.js
@@ -3,9 +3,9 @@ const runAxios = async () => {
     //CRIA ELEMENTO DE ESPERAR CARREGAR
     document.querySelector('#myList').innerHTML = `<div class='p-3 mt-5'>
                                                     <h3>Carregando 
-                                                        <div class='spinner-grow text-success'></div>
-                                                        <div class='spinner-grow text-success'></div>
-                                                        <div class='spinner-grow text-success'></div>
+                                                        <div id="dv1"></div>
+                                                        <div id="dv2"></div>
+                                                        <div id="dv3"></div>
                                                     </h3>
                                                 </div>`;
     await dados();
@@ -19,19 +19,26 @@ function dados() {
         .then(function (response) {
             //VARRE TODOS OS DADOS
             response.data.forEach(value => {
-                var { name, description, html_url } = value;
-
-                var content = document.querySelector('#myList');
-                if (cont === 0) {
-                    content.innerHTML = '';
+                var { name, description } = value;
+                //Igonara Repositorio home
+                if (name !== "GiaconBruno.github.io") {
+                    var content = document.querySelector('#myList');
+                    //Valida se first value
+                    if (cont === 0) {
+                        content.innerHTML = '';
+                    }
+                    //Cria Elemento 'li'
+                    var li = document.createElement('li');
+                    //Define Elemento 'li' dentro do Elemento '#myList'
+                    content.appendChild(li);
+                    //Criar novo registro
+                    var list = `<i class="fas fa-check mr-3 text-success"></i>
+                                <a href="https://GiaconBruno.github.io/${name}" target="_blank">${name}</a>
+                                 - ${description}`;
+                    //Define novo registro dentro do 'li'
+                    li.innerHTML = list;
+                    cont++;
                 }
-                console.log(name);
-
-                var li = document.createElement('li');
-                content.appendChild(li);
-                var list = `<i class="fas fa-check mr-3"></i><a href="../${name}">${name}</a> - ${description}`;
-                li.innerHTML = list;
-                cont++;
             });
         })
         .catch(function (error) {
---
+++ js/main.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+    document.querySelector('#dv1').classList = "spinner-grow text-success";
+}, 0);
+setTimeout(function () {
+    document.querySelector('#dv2').classList = "spinner-grow text-success";
+}, 100);
+setTimeout(function () {
+    document.querySelector('#dv3').classList = "spinner-grow text-success";
+}, 200);
