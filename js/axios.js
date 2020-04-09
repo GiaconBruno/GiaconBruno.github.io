64d187c58b786b0a31ed039f7e7be8ca957cbecf
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-09 03:30:07 -0300

    att

--- js/axios.js
+++ js/axios.js
@@ -19,7 +19,9 @@ function dados() {
         .then(function (response) {
             //VARRE TODOS OS DADOS
             response.data.forEach(value => {
-                var { name, description } = value;
+                var { name, description, updated_at } = value;
+                updated_at = updated_at.substr(0, 10);
+                updated_at = updated_at.slice(8, 10) + "/" + updated_at.slice(5, 7) + "/" + updated_at.slice(0, 4);
                 //Igonara Repositorio home
                 if (name !== "GiaconBruno.github.io") {
                     var content = document.querySelector('#myList');
@@ -28,21 +30,28 @@ function dados() {
                         content.innerHTML = '';
                     }
                     //Cria Elemento 'li'
-                    var li = document.createElement('li');
+                    var div = document.createElement('div');
                     //Define Elemento 'li' dentro do Elemento '#myList'
-                    content.appendChild(li);
+                    content.appendChild(div);
                     //Criar novo registro
-                    var list = `<i class="fas fa-check mr-3 text-success"></i>
-                                <a href="https://GiaconBruno.github.io/${name}" target="_blank">${name}</a>
-                                 - ${description}`;
+                    var list = `<a href="https://GiaconBruno.github.io/${name}" target="_blank">
+                                    <div class="item text-break">
+                                        <div class="d-flow">
+                                            <i class="fas fa-check mr-3 mt-2 text-success float-left fa-lg"></i>
+                                            <span class="float-right">${updated_at} </span>
+                                            <span class="float-left"><strong>${name}</strong></span>
+                                        </div>
+                                        <span> - ${description}</span>
+                                     </div>
+                                </a>`;
                     //Define novo registro dentro do 'li'
-                    li.innerHTML = list;
+                    div.innerHTML = list;
                     cont++;
                 }
             });
         })
         .catch(function (error) {
-            document.querySelector('#myList').innerHTML = `<div class="m-3">
+            document.querySelector('#myList').innerHTML = `<div class="m-3 text-break">
                                                                 <h2>Erro ao carregar projetos</h2>
                                                                 <span>${error}</span>
                                                             </div>`;
