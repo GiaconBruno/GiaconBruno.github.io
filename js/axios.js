14f5113ee47f5438fcc5519403b0a68e817e8c9b
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-10 03:06:49 -0300

    att

--- js/axios.js
+++ js/axios.js
@@ -1,11 +1,14 @@
 //CRIA FUNCAO ASSINCRONA
 const runAxios = async () => {
     //CRIA ELEMENTO DE ESPERAR CARREGAR
-    document.querySelector('#myList').innerHTML = `<div class='p-3 mt-5'>
-                                                    <h3>Carregando 
-                                                        <div id="dv1"></div>
-                                                        <div id="dv2"></div>
-                                                        <div id="dv3"></div>
+    document.querySelector('#myList').innerHTML = `<div class='mt-3'>
+                                                    <h3 class="text-left">
+                                                        <span>Carregando</span>
+                                                            <div class="d-inline-flex min-w">
+                                                                <div id="dv1"> </div>
+                                                                <div id="dv2"> </div>
+                                                                <div id="dv3"> </div>
+                                                            </div>
                                                     </h3>
                                                 </div>`;
     await dados();
@@ -19,9 +22,17 @@ function dados() {
         .then(function (response) {
             //VARRE TODOS OS DADOS
             response.data.forEach(value => {
-                var { name, description, updated_at } = value;
+                var { name, description, updated_at, has_pages } = value;
+                //Formatando Data
                 updated_at = updated_at.substr(0, 10);
                 updated_at = updated_at.slice(8, 10) + "/" + updated_at.slice(5, 7) + "/" + updated_at.slice(0, 4);
+                //Verificando se existe page.io
+                var icon, link;
+                (has_pages === true) ? icon = 'fa-check' : icon = 'fa-times negative';
+                (has_pages === true) ? link = 'https://GiaconBruno.github.io/' : link = 'https://github.com/giaconbruno/';
+                //Removendo descricao null
+                (description === null) ? description = 'Sem descrição' : description;
+
                 //Igonara Repositorio home
                 if (name !== "GiaconBruno.github.io") {
                     var content = document.querySelector('#myList');
@@ -34,14 +45,14 @@ function dados() {
                     //Define Elemento 'li' dentro do Elemento '#myList'
                     content.appendChild(div);
                     //Criar novo registro
-                    var list = `<a href="https://GiaconBruno.github.io/${name}" target="_blank">
+                    var list = `<a href="${link + name}" target="_blank">
                                     <div class="item text-break">
-                                        <div class="d-flow">
-                                            <i class="fas fa-check mr-3 mt-2 text-success float-left fa-lg"></i>
-                                            <span class="float-right">${updated_at} </span>
-                                            <span class="float-left"><strong>${name}</strong></span>
+                                        <div>
+                                            <i class="mr-3 mt-2 text-success fas ${icon} fa-lg"></i>
+                                            <span class="d-flow float-right">${updated_at} </span>
+                                            <span class="text-break"><strong>${name}</strong></span>
                                         </div>
-                                        <span> - ${description}</span>
+                                        <span class="text-break"> - ${description}</span>
                                      </div>
                                 </a>`;
                     //Define novo registro dentro do 'li'
@@ -51,9 +62,10 @@ function dados() {
             });
         })
         .catch(function (error) {
+            (error.message === 'Request failed with status code 404') ? error = 'Projetos Indisponiveis no momento' : error;
             document.querySelector('#myList').innerHTML = `<div class="m-3 text-break">
                                                                 <h2>Erro ao carregar projetos</h2>
                                                                 <span>${error}</span>
-                                                            </div>`;
+                                                            </div>`
         });
 };
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
+
+document.querySelector('#dv1').classList = ("ml-2 text-success spinner-grow");
 setTimeout(function () {
-    document.querySelector('#dv1').classList = "spinner-grow text-success";
-}, 0);
-setTimeout(function () {
-    document.querySelector('#dv2').classList = "spinner-grow text-success";
+    document.querySelector('#dv2').classList = ("ml-2 text-success spinner-grow");
 }, 125);
 setTimeout(function () {
-    document.querySelector('#dv3').classList = "spinner-grow text-success";
+    document.querySelector('#dv3').classList = ("ml-2 text-success spinner-grow");
 }, 200);
